2319bb3a4ec60d7f8a0b5186780e73165c94e141
Ali sorathiya <alisorathiya970@gmail.com>
2026-06-02 11:26:58 -0700

    Update app.js

--- notification-service/app.js
+++ notification-service/app.js
@@ -1,29 +1,3 @@
-// import express from "express"
-// import connectDb from "./db/Db.js";
-// import router from "./routes/user.route.js";
-
-// const app = express();
-
-// const PORT = process.env.PORT  || 8000 ;
-
-
-// connectDb();
-
-
-// app.use(express.json())
-
-
-
-// app.use("/api/v1",router)
-
-
-// app.get("/",(req,res)=>{
-//     res.send("Hello from my user service")
-// })
-
-
-// app.listen(PORT ,()=> console.log(` User service server started on ${PORT}`))
-
 
 
 import amqp from "amqplib"
